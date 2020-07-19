5e089304508128d050e43e94fe3926e2ed31f657
lixingyang <176840078@qq.com>
2020-07-19 21:20:36 +0800

    李：补充备案号

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -10,5 +10,8 @@ export default function(){
             subTitle="欢迎来到“哈哈学习”，你能来这里，说明你智商蛮可以！"
             extra={<Button type="primary">Back Home</Button>}
         />
+        <footer className="be-center">
+            网站备案/许可证号：陕ICP备20008324号
+        </footer>
     </div>
 }
