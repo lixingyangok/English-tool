3be67ffa0d3eb011ad7ef48b3fd569a83b90aefe
lixingyang <176840078@qq.com>
2021-02-14 20:12:23 +0800

    修改公共方法

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -2,7 +2,7 @@
  * @Author: 李星阳
  * @Date: 2021-02-14 15:52:51
  * @LastEditors: 李星阳
- * @LastEditTime: 2021-02-14 20:09:29
+ * @LastEditTime: 2021-02-14 20:11:23
  * @Description: 
  */
 
@@ -20,10 +20,34 @@ export const trainingDB = (()=>{
 })();
 
 export const wordsDB = (()=>{
-	console.log('trainingDB ▲▲:\n', new Date().toLocaleString());
+	console.log('wordsDB ▲▲:\n', new Date().toLocaleString());
 	const oResult = new window.Dexie("wordsDB");
 	aAlphabet.forEach((cur, idx)=>{
 		oResult.version(idx+1).stores({[cur]: '++id, word'});
 	});
 	return oResult;
 })();
+
+export function mix(...mixins) {
+	class Mix {
+		constructor() {
+			for (let mixin of mixins) {
+				copyProperties(this, new mixin()); // 拷贝实例属性
+			}
+		}
+	}
+	for (let mixin of mixins) {
+		copyProperties(Mix, mixin); // 拷贝静态属性
+		copyProperties(Mix.prototype, mixin.prototype); // 拷贝原型属性
+	}
+	return Mix;
+}
+
+function copyProperties(target, source) {
+	for (let key of Reflect.ownKeys(source)) {
+		if (key !== 'constructor' && key !== 'prototype' && key !== 'name' ) {
+			let desc = Object.getOwnPropertyDescriptor(source, key);
+			Object.defineProperty(target, key, desc);
+		}
+	}
+}
