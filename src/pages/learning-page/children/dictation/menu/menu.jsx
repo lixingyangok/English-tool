b283cd6561c49e015fcb45221f5c36d377dcd6d1
lixingyang <176840078@qq.com>
2021-02-15 13:28:25 +0800

    修改工具条的样式

--- src/pages/learning-page/children/dictation/menu/menu.jsx
+++ src/pages/learning-page/children/dictation/menu/menu.jsx
@@ -1,41 +1,25 @@
 import React from "react";
 import * as cpnt from './style/menu.js';
 
-/* 
-文件：导入、导出、暂存、最近打开
-编辑：撤销、恢复、查找/替换
-字幕：拆分、合并、时间调整
-关于
-*/
 export default class Menu extends React.Component{
 	render(){
 		const {commander} = this.props;
 		const btnBar = <cpnt.BtnBar>
-			<span className="btn" title="显示词库"
-				onClick={()=>commander('showWordsDialog')}
-			>
-				<i className="fas fa-book"/>
-			</span>
-			<span className="btn" title="保存字幕到浏览器" 
-				onClick={()=>commander('toSaveInDb')}
-			>
-				<i className="fas fa-save"/>
-			</span>
-			<span className="btn" title="导出字幕到本地" 
-				onClick={()=>commander('toExport')}
-			>
-				<i className="fas fa-download"/>
-			</span>
-			<span className="btn" title="上传字幕到云"
-				onClick={()=>commander('uploadToCloudBefore')}
-			>
-				<i className="fas fa-cloud-upload-alt"/>
-			</span>
-			<span className="btn" title="对比两地字幕"
-				onClick={()=>commander('compareSubtitle')}
-			>
-				<i className="fas fa-columns"/>
-			</span>
+			<cpnt.Btn onClick={()=>commander('showWordsDialog')} >
+				<i className="fas fa-book"/>显示词库
+			</cpnt.Btn>
+			<cpnt.Btn onClick={()=>commander('compareSubtitle')} >
+				<i className="fas fa-columns"/>对比两地字幕
+			</cpnt.Btn>
+			<cpnt.Btn onClick={()=>commander('toSaveInDb')} >
+				<i className="fas fa-save"/>保存字幕到浏览器
+			</cpnt.Btn>
+			<cpnt.Btn onClick={()=>commander('uploadToCloudBefore')} >
+				<i className="fas fa-cloud-upload-alt"/>上传字幕到云
+			</cpnt.Btn>
+			<cpnt.Btn onClick={()=>commander('toExport')} >
+				<i className="fas fa-download"/>导出字幕到本地
+			</cpnt.Btn>
 		</cpnt.BtnBar>
 		return <cpnt.Header>
 			{btnBar}
--- src/pages/learning-page/children/dictation/menu/style/menu.js
+++ src/pages/learning-page/children/dictation/menu/style/menu.js
@@ -9,37 +9,24 @@ export const Header = styled.header`
 	background: white;
 	box-shadow: 0px 1px 3px rgba(0,0,0,0.2);
 	background: #f0f0f0;
-`;
-
-export const Nav = styled.nav`
-	background: white;
-	box-shadow: 0px -2px 0px #f2f2f2;
-	border-bottom: solid 1px #a0a0a0;
-`;
-
-export const Ul = styled.ul`
-	display: flex;
-	margin: 0;
-	li{
-		line-height: 2;
-		margin-right: 15px;
-		cursor: pointer;
-	}
+	padding: 6px 15px;
 `;
 
 export const BtnBar = styled.section`
-	padding: 8px 0;
 	display: flex;
-	.btn{
-		width: 36px;
-		text-align: center;
-		cursor: pointer;
-		&:Hover{
-			color: blue;
-		}
+`;
+
+export const Btn = styled.span`
+	vertical-align: middle;
+	cursor: pointer;
+	margin-right: 18px;
+	color: #aaa;
+	font-size: 12px;
+	&:Hover{
+		color: #333;
 	}
 	.fas{
-		font-size: 16px;
-		vertical-align: middle;
+		margin: 0 0.4em 0 0;
 	}
 `;
+
