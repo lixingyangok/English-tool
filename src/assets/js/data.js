dbb33000d3949a5c8b55cd963ebe25b651e8eb07
lixingyang <176840078@qq.com>
2021-02-21 16:10:17 +0800

    添加新文件

---
+++ src/assets/js/data.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: 李星阳
+ * @Date: 2021-02-21 16:06:12
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2021-02-21 16:09:11
+ * @Description: 
+ */
+
+export const oType = {
+    '-1': {name: '全部', iCode: -1 },
+    '0': {name: '默认', iCode: 0 },
+    '1': {name: '听写', iCode: 1 },
+    '2': {name: '阅读', iCode: 2 },
+}
---
+++ src/assets/js/global.js
@@ -0,0 +1,105 @@
+/*
+ * @Author: 李星阳
+ * @Date: 2021-02-21 14:31:01
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2021-02-21 14:35:19
+ * @Description: 
+ */
+
+import axios from 'assets/js/ajax.js';
+const store = require('store');
+
+export default function (){
+	Object.defineProperties(window, {
+		mix: {
+			writable: false,
+			value: mix,
+		},
+		store: {
+			writable: false,
+			value: store,
+		},
+		axios: {
+			writable: false,
+			value: axios,
+		},
+	});
+
+	Object.defineProperties(Object.prototype, { // eslint-disable-line
+		dc_: { // dc = deep copy = 深拷贝
+			get: function () {
+				// console.time('%c克隆耗时■■■');
+				function toClone(source) {
+					const isNeed = source && typeof source == 'object' && source instanceof Object;
+					if (!isNeed) return source; //不处理值类型，即忽略非数组、非对象
+					const newObj = new source.constructor();
+					const iterator = Array.isArray(source) ? source.entries() : Object.entries(source);
+					for (let [key, val] of iterator) {
+						newObj[key] = val instanceof Object ? toClone(val) : val;
+					}
+					return newObj;
+				}
+				const result = (() => {
+					try {return toClone(this)} 
+					catch (err) {console.error('拷贝没成功', err)}
+					return JSON.parse(JSON.stringify(this));
+				})();
+				// console.timeEnd('%c克隆耗时■■■');
+				return result;
+			},
+		},
+	});
+
+	Object.defineProperties(Array.prototype, { // eslint-disable-line
+		last_: { // 取数组末位
+			get: function () {
+				return this[this.length - 1] || null;
+			},
+		},
+	});
+
+	Date.prototype.format = function (fmt) {  // eslint-disable-line
+		var o = {
+			"M+": this.getMonth() + 1,                 //月份 
+			"d+": this.getDate(),                    //日 
+			"h+": this.getHours(),                   //小时 
+			"m+": this.getMinutes(),                 //分 
+			"s+": this.getSeconds(),                 //秒 
+			"q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+			"S": this.getMilliseconds()             //毫秒 
+		};
+		if (/(y+)/.test(fmt)) {
+			fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+		}
+		for (var k in o) {
+			if (new RegExp("(" + k + ")").test(fmt)) {
+				fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+			}
+		}
+		return fmt;
+	}
+}
+
+function copyProperties(target, source) {
+	for (let key of Reflect.ownKeys(source)) {
+		if (key !== 'constructor' && key !== 'prototype' && key !== 'name' ) {
+			let desc = Object.getOwnPropertyDescriptor(source, key);
+			Object.defineProperty(target, key, desc);
+		}
+	}
+}
+
+function mix(...mixins) {
+	class Mix {
+		constructor() {
+			for (let mixin of mixins) {
+				copyProperties(this, new mixin()); // 拷贝实例属性
+			}
+		}
+	}
+	for (let mixin of mixins) {
+		copyProperties(Mix, mixin); // 拷贝静态属性
+		copyProperties(Mix.prototype, mixin.prototype); // 拷贝原型属性
+	}
+	return Mix;
+}
